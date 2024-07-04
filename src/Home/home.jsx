5497346faa0dffa140bdcc1386cf9507fae5dab7
sameerg1810 <64128604+sameerg1810@users.noreply.github.com>
2024-07-04 15:32:20 +0530

    changed the index.css file

--- src/Home/home.jsx
+++ src/Home/home.jsx
@@ -1,23 +1,21 @@
-import React from 'react'
-import Maincategory from './maincategory'
-import './home.css'
-import Howitworks from './howitworks'
-import Mostbookedservices from './MOST-BOOKED-SERVICES/mostbookedservices'
-import Ourcoreservices from './OUR-CORE-SERVICES/our-core-services'
-
+import React from "react";
+import Maincategory from "./maincategory";
+import "./home.css";
+import Howitworks from "./howitworks";
+import Mostbookedservices from "./MOST-BOOKED-SERVICES/mostbookedservices";
+import Ourcoreservices from "./OUR-CORE-SERVICES/our-core-services";
 
 const Home = () => {
   return (
-     <> 
-      <div className='home-main'>
-        <Maincategory/> 
-        <Mostbookedservices/>
-         <Howitworks/>
-         <Ourcoreservices/>
-
+    <>
+      <div className="home-main">
+        <Maincategory />
+        <Mostbookedservices />
+        <Howitworks />
+        <Ourcoreservices />
       </div>
-     </>
-  )
-}
+    </>
+  );
+};
 
-export default Home
+export default Home;
