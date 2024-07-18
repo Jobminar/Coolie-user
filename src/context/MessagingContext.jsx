833de0bc19f662db99bb24aff7310452b53c61d6
sameerg1810 <64128604+sameerg1810@users.noreply.github.com>
2024-07-18 17:22:39 +0530

    added the messaging context for firebase webpush

--- src/context/MessagingContext.jsx
+++ src/context/MessagingContext.jsx
@@ -40,7 +40,7 @@ export const MessagingProvider = ({ children }) => {
     // Handle incoming messages. Called when:
     // - a message is received while the app has focus
     // - the user clicks on an app notification created by a service worker
-    //   `messaging.onBackgroundMessage` handler.
+    //   `messaging.onBackgroundMessage` handler...
     onMessage(messaging, (payload) => {
       console.log("Message received. ", payload);
       // You can update your UI or display a notification here.
