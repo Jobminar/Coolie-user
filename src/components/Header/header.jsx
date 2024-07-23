725702812b864daa6a8e0fc785a9c5d74136935e
sameerg1810 <64128604+sameerg1810@users.noreply.github.com>
2024-07-23 10:27:48 +0530

    added the header and workcomponent

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -11,7 +11,6 @@ import profile from "../../assets/images/profile.png";
 import location from "../../assets/images/location-marker.png";
 
 const Header = () => {
-  const locationname = "Hyderabad";
   const navigate = useNavigate();
 
   const navigateToWorkers = () => {
@@ -19,40 +18,38 @@ const Header = () => {
   };
 
   return (
-    <>
-      <div className="main-h">
-        <div className="f-h">
-          <div className="f-h-icons">
-            <img src={apple} alt="apple-icon" />
-            <img src={playstore} alt="play-store-icon" />
-            <p>Download Mobile App</p>
-          </div>
-          <div className="f-h-last-icons">
-            <img src={help} alt="icon" />
-            <img src={translate} alt="icon" />
-            <img src={profile} alt="icon" />
-          </div>
+    <div className="main-h">
+      <div className="f-h">
+        <div className="f-h-icons">
+          <img src={apple} alt="apple-icon" />
+          <img src={playstore} alt="play-store-icon" />
+          <p>Download Mobile App</p>
         </div>
-        <div className="s-h">
-          <div className="s-h-logo">
-            <img src={logo} alt="logo" />
+        <div className="f-h-last-icons">
+          <img src={help} alt="help-icon" />
+          <img src={translate} alt="translate-icon" />
+          <img src={profile} alt="profile-icon" />
+        </div>
+      </div>
+      <div className="s-h">
+        <div className="s-h-logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="s-h-s">
+          <div className="location">
+            <img src={location} alt="location-icon" />
+            <input placeholder="Hyderabad" />
           </div>
-          <div className="s-h-s">
-            <div className="location">
-              <img src={location} alt="location" />
-              <input placeholder="Hyderabad" />
-            </div>
-            <div className="search-header">
-              <input placeholder="search for a service ex: Room cleaning , kitchen cleaning " />
-            </div>
-            <button className="books-button">Book a Service</button>
-            <div className="user-icon" onClick={navigateToWorkers}>
-              <Person2OutlinedIcon />
-            </div>
+          <div className="search-header">
+            <input placeholder="search for a service ex: Room cleaning, kitchen cleaning" />
+          </div>
+          <button className="books-button">Book a Service</button>
+          <div className="user-icon" onClick={navigateToWorkers}>
+            <Person2OutlinedIcon />
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
